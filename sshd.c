14b59bf44214a9f46d6f0b5281f94420ef0fc888
djm <djm@openbsd.org>
2026-09-15 06:49:51 +0000

    wrap line

--- sshd.c
+++ sshd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshd.c,v 1.629 2026/09/06 18:36:24 deraadt Exp $ */
+/* $OpenBSD: sshd.c,v 1.631 2026/09/15 06:49:51 djm Exp $ */
 /*
  * Copyright (c) 2000, 2001, 2002 Markus Friedl.  All rights reserved.
  * Copyright (c) 2002 Niels Provos.  All rights reserved.
@@ -1244,6 +1244,18 @@ prepare_proctitle(int ac, char **av)
 	return ret;
 }
 
+/* Disconnect from the controlling tty. */
+void
+disconnect_controlling_tty(void)
+{
+	int fd;
+
+	if ((fd = open(_PATH_TTY, O_RDWR | O_NOCTTY)) >= 0) {
+		(void) ioctl(fd, TIOCNOTTY, NULL);
+		close(fd);
+	}
+}
+
 static void
 print_config(struct connection_info *connection_info)
 {
@@ -1384,7 +1396,8 @@ main(int ac, char **av)
 			have_connection_info = 1;
 			break;
 		case 'u':
-			utmp_len = (u_int)strtonum(optarg, 0, HOST_NAME_MAX+1+1, NULL);
+			utmp_len = (u_int)strtonum(optarg, 0,
+			    HOST_NAME_MAX+1+1, NULL);
 			if (utmp_len > HOST_NAME_MAX+1) {
 				fprintf(stderr, "Invalid utmp length.\n");
 				exit(1);
